1f49432384738adfb87117a79c52d8f9df6aed24
ready4exam <ready4urexam@gmail.com>
2025-12-31 06:20:04 +0530

    chore: update table_id for Fibre to Fabric → science_fibre_fabric_6_quiz

--- js/curriculum.js
+++ js/curriculum.js
@@ -52,7 +52,7 @@ export const curriculum = {
       },
       {
         "chapter_title": "Fibre to Fabric",
-        "table_id": "",
+        "table_id": "science_fibre_fabric_6_quiz",
         "section": "Science"
       },
       {
