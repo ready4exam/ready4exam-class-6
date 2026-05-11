2d476182fc75c2fd515a319773f6bdd685214a70
ready4exam <ready4urexam@gmail.com>
2026-05-11 13:16:08 +0530

    chore: update table_id for Major Domains of the Earth → social_major_earth_6_quiz

--- js/curriculum.js
+++ js/curriculum.js
@@ -215,7 +215,7 @@ export const curriculum = {
       },
       {
         "chapter_title": "Major Domains of the Earth",
-        "table_id": "",
+        "table_id": "social_major_earth_6_quiz",
         "section": "Social Science"
       },
       {
